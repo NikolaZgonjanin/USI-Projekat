630a36c6afec26aedf2d5233a00fe165b24e0640
Nikola Zgonjanin <zgonjanin.nikola.92@gmail.com>
2026-01-14 15:33:39 +0100

    Prosirena tabela projekata na punu sirinu layout-a

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-10 max-w-5xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-10 max-w-7xl mx-auto w-full sm:px-6 lg:px-8">
         @if (session('success'))
             <div class="mb-6 rounded-md bg-green-100 border border-green-300 px-4 py-2 text-green-800">
                 {{ session('success') }}
